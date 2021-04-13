2bcd0c930147d1540a4c9d475a0c721460f3c77c
andrescaro189 <andrescaro189@gmail.com>
2021-04-13 16:58:28 -0500

    Se crea el setup del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularFireModule} from '@angular/fire';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyAReIfC3I0ovui0RoOUazbD87XDv2-_9TA',
+  authDomain: 'sofkachat-team-5ee74.firebaseapp.com',
+  projectId: 'sofkachat-team-5ee74',
+  storageBucket: 'sofkachat-team-5ee74.appspot.com',
+  messagingSenderId: '82160573604',
+  appId: '1:82160573604:web:cdd3dfd08356851b9b9a31'
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    //HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
